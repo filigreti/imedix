304be0a088ae94a9febb00ebc99457a2e11cca95
filigreti <aolfiligre@gmail.com>
2021-01-03 17:27:27 +0100

    WIP

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './assets/index.css';
+// import BackButton from '@/components/BackButton';
+// import Modal from '@/components/Modal';
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App);
+
+// app.component('refer-back-button', BackButton);
+// app.component('refer-modal', Modal);
+
+app.use(store);
+app.use(router);
+app.mount('#app');
